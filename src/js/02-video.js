690d9661bc8a5f3fea9e195e97b817f23599a1ff
Denix200010 <103590306+Denix200010@users.noreply.github.com>
2022-09-05 14:23:20 +0300

    +

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,15 +1,34 @@
-import Player from "@vimeo/player";
-console.log("Player", Player)
+import Player from '@vimeo/player';
 
+import throttle from 'lodash.throttle';
 
-import throttle from "lodash.throttle";
-    
 const iframe = document.querySelector('iframe');
-    
-    const iframePlayer = new Vimeo.Player(iframe);
-    const SAVE_TIMES = "videoplayer-current-time";
 
-//     const onPlay = function (data) {
-//   const stringifyData = JSON.stringify(data);
-//   localStorage.setItem(SAVE_TIMES, stringifyData);
-//     };
+const iframePlayer = new Vimeo.Player(iframe);
+const SAVE_TIME = 'videoplayer-current-time';
+
+const onPlay = function (data) {
+  localStorage.setItem(SAVE_TIME, JSON.stringify(data));
+};
+iframePlayer.on('timeupdate', throttle(onPlay, 1000));
+resume();
+
+function resume() {
+  if (JSON.parse(localStorage.getItem(SAVE_TIME)) === null) {
+    return;
+  }
+  const time = JSON.parse(localStorage.getItem(SAVE_TIME))['seconds'];
+  if (time) {
+    iframePlayer
+      .setCurrentTime(time)
+      .then(function (seconds) {})
+      .catch(function (error) {
+        switch (error.name) {
+          case 'RangeError':
+            break;
+          default:
+            break;
+        }
+      });
+  }
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+import throttle from "lodash.throttle";
+
+const formRef = document.querySelector('.feedback-form');
+const localStorageData = {};
+
+formRef.addEventListener('input', throttle(onFormInput, 500));
+formRef.addEventListener('submit', onFormSubmit);
+populateOutput();
+
+function onFormInput(e) {
+    console.log(1)
+    localStorageData[e.target.name] = e.target.value;
+    localStorage.setItem('feedback-form-state', JSON.stringify(localStorageData));
+    
+}
+
+function onFormSubmit(e) {
+    e.preventDefault();
+    formRef.reset();
+    localStorage.removeItem('feedback-form-state');
+    console.log(localStorageData);
+}
+
+function populateOutput() {
+    const parseData = JSON.parse(localStorage.getItem('feedback-form-state'));
+    if (parseData === null) {
+        return;
+    }
+    formRef[0].value = parseData.email || '';
+    formRef[1].value = parseData.message || '';
+}
 
