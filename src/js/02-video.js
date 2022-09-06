7587a17f5ce6f2d6e8e9579762bf8732e0d42fe5
Denix200010 <103590306+Denix200010@users.noreply.github.com>
2022-09-06 16:58:22 +0300

    edited vimeo lib

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,17 +1,14 @@
 import Player from "@vimeo/player";
-
-import throttle from "lodash.throttle";
-
-
+console.log("Player", Player)
+import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
-    
-    const iframePlayer = new Vimeo.Player(iframe);
+    const player = new Player(iframe);
     const SAVE_TIME = "videoplayer-current-time";
 
     const onPlay = function (data) {
     localStorage.setItem(SAVE_TIME, JSON.stringify(data));
     };
-iframePlayer.on('timeupdate', throttle(onPlay, 1000));
+player.on('timeupdate', throttle(onPlay, 1000));
 resume();
     
 function resume() {
@@ -20,7 +17,7 @@ function resume() {
   }
   const time = JSON.parse(localStorage.getItem(SAVE_TIME))['seconds'];
   if (time) {
-    iframePlayer
+    player
       .setCurrentTime(time)
       .then(function (seconds) {})
       .catch(function (error) {
